b5b3762e3975ab14cffe0fb7119b931d6ca1f361
Hadley Rich <hads@nice.net.nz>
2015-11-27 21:28:04 +1300

    Move templates from four to two space tabs

--- gerblook/static/base.js
+++ gerblook/static/base.js
@@ -28,51 +28,52 @@ jQuery.fn.serializeObject = function() {
 };
 
 $(function() {
-    tab_history();
-    $('input[type=file]').bootstrapFileInput();
-/*    $('#zipfile').ajaxfileupload({
-        action: '/',
-        validate_extensions: false,
-        submit_button: $('#go'),
-        onStart: function() {
-            var elem = $(this);
-            var fields = $('#gerber-form').serializeObject();
-            elem.before(function() {
-              var key, html = '';
-              for(key in fields) {
-                var paramVal = fields[key];
-                if (typeof paramVal === 'function') {
-                  paramVal = paramVal();
-                }
-                html += '<input type="hidden" name="' + key + '" value="' + paramVal + '" />';
-              }
-              return html;
-            });
-            console.log(fields);
+  tab_history();
+  $('input[type=file]').bootstrapFileInput();
+/*
+  $('#zipfile').ajaxfileupload({
+    action: '/',
+    validate_extensions: false,
+    submit_button: $('#go'),
+    onStart: function() {
+      var elem = $(this);
+      var fields = $('#gerber-form').serializeObject();
+      elem.before(function() {
+        var key, html = '';
+        for(key in fields) {
+          var paramVal = fields[key];
+          if (typeof paramVal === 'function') {
+            paramVal = paramVal();
+          }
+          html += '<input type="hidden" name="' + key + '" value="' + paramVal + '" />';
         }
-    });
+        return html;
+      });
+      console.log(fields);
+    }
+  });
 */
 });
 
 function tab_history(selector) {
-    if (selector == undefined) {
-        selector = "";
+  if (selector == undefined) {
+    selector = "";
+  }
+
+  /* Automagically jump on good tab based on anchor */
+  $(document).ready(function() {
+    url = document.location.href.split('#');
+    if(url[1] != undefined) {
+      $(selector + '[href=#'+url[1]+']').tab('show');
     }
- 
-    /* Automagically jump on good tab based on anchor */
-    $(document).ready(function() {
-        url = document.location.href.split('#');
-        if(url[1] != undefined) {
-            $(selector + '[href=#'+url[1]+']').tab('show');
-        }
-    });
- 
-    var update_location = function(event) {
-        document.location.hash = this.getAttribute("href");
-    }
- 
-    /* Update hash based on tab */
-    $(selector + "[data-toggle=pill]").click(update_location);
-    $(selector + "[data-toggle=tab]").click(update_location);
+  });
+
+  var update_location = function(event) {
+    document.location.hash = this.getAttribute("href");
+  }
+
+  /* Update hash based on tab */
+  $(selector + "[data-toggle=pill]").click(update_location);
+  $(selector + "[data-toggle=tab]").click(update_location);
 }
 
